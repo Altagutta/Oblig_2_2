4d63adf3e93835e65d7c600f826422dde4bcb1b0
fvu000 <fvu000@post.uit.no>
2018-10-11 12:03:58 +0200

    creating new tests

--- src/BSTTest.java
+++ src/BSTTest.java
@@ -5,33 +5,45 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BSTTest {
-    static BST<Integer> defaultIntList;
+    static BST<Integer> PerfectBST;
+    static BST<Integer> ImperfectBST;
 
     @Before
-    public void initStaticList() {
-        defaultIntList = new BST<>(new Integer[]{10, 5, 12, 7, 3, 20, 11});
+    public void initTrees() {
+        PerfectBST = new BST<>(new Integer[]{10, 5, 12, 7, 3, 20, 11});
+        ImperfectBST = new BST<>(new Integer[]{10, 5, 12, 7, 3, 20, 11, 9});
     }
 
     @Test
     public void isPerfectMethod(){
-        assertEquals(true, defaultIntList.isPerfect());
+        assertEquals(true, PerfectBST.isPerfect());
+        assertEquals(false, ImperfectBST.isPerfect());
     }
 
     @Test
     public void countNodesMethod(){
-        assertEquals(7, defaultIntList.countNodes());
+        assertEquals(7, PerfectBST.countNodes());
+        assertEquals(8, ImperfectBST.countNodes());
     }
     @Test
     public void countHeightMethod(){
-        assertEquals(3, defaultIntList.height());
+        assertEquals(3, PerfectBST.height());
+        assertEquals(3, ImperfectBST.height());
     }
     @Test
     public void getNoOfLeavesMethod(){
-        assertEquals(4, defaultIntList.getNoOfLeaves());
+        assertEquals(4, PerfectBST.getNoOfLeaves());
+        assertEquals(4, ImperfectBST.height());
     }
     @Test
     public void getNoOfNonLeavesMethod(){
-        assertEquals(3, defaultIntList.getNoOfNonLeaves());
+        assertEquals(3, PerfectBST.getNoOfNonLeaves());
+        assertEquals(4, ImperfectBST.getNoOfNonLeaves());
+    }
+
+    @Test
+    public void inorderIterativeMethod(){
+
     }
 }
 //  </FREDRIK>
